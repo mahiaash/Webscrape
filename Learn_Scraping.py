165b5401c5629f4fdcae85a1faa976dacc1a436c
mahiaash <mahiaash@buffalo.edu>
2021-11-13 17:21:49 -0500

    Insert author function

--- Learn_Scraping.py
+++ Learn_Scraping.py
@@ -7,33 +7,17 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from bs4 import BeautifulSoup
 
-#from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
-#https://www.udemy.com/courses/free/?lang=en&sort=popularity
-
-sort_by_type = 'newest'
 
 #firefox_driver_path = '/home/mahia/Projects/Webscrape/geckodriver.exe'
 
-delay = 15
 
-#driver = webdriver.Firefox(executable_path=firefox_driver_path)
-
-driver = webdriver.Firefox(())
-
-driver.get("https://www.udemy.com/courses/free/?lang=en&p=10&sort=newest")
-
-try:
-        WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CLASS_NAME, 'course-list--container--3zXPS')))
-except TimeoutException:
-    
-    print('Loading exceeds delay time')
-    
-else:
-    with open("markup.html","w", encoding="utf-8") as file:
+with open("markup.html","w", encoding="utf-8") as file:
         file.write(driver.page_source)
-finally:
-    driver.quit()
-# for page_number in range(1,12):
 
-#     page_url = f"https://www.udemy.com/courses/free/?lang=en&p={page_number}&sort=newest" 
+#Here we are creating a function that will insert the authors into the search bar
+def insert_author():
+    with open("research_docs.txt","r") as files:
+        lines = files.readlines()
+        print(lines)
+
